8ed0aa03aa3004c6a91e643462f7e603016f3c66
Mohit Kumawat <115658001+mo-heatt@users.noreply.github.com>
2023-05-08 19:12:19 +0000

    sendEmail modified

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -2,24 +2,25 @@ const sgMail =  require('@sendgrid/mail')
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const sendEmail = async (options)=>{
-    const transporter = nodeMailer.createTransport({
-        host: process.env.SMPT_HOST,
-        port: process.env.SMPT_PORT,
-        service: process.env.SMPT_SERVICE, /* The service provides a range of features, such as email templating, recipient management, and reporting, that make it easy for developers to send emails and track their delivery and engagement. */
-        auth: {
-            user: process.env.SMPT_MAIL,
-            pass: process.env.SMPT_PASSWORD,
-        },
-    });
 
-    const mailOptions = {
-        from: process.env.SMPT_MAIL,
-        to: options.email,
-        subject: options.subject,
-        html: options.message,
-    };
+    // const transporter = nodeMailer.createTransport({
+    //     host: process.env.SMPT_HOST,
+    //     port: process.env.SMPT_PORT,
+    //     service: process.env.SMPT_SERVICE, /* The service provides a range of features, such as email templating, recipient management, and reporting, that make it easy for developers to send emails and track their delivery and engagement. */
+    //     auth: {
+    //         user: process.env.SMPT_MAIL,
+    //         pass: process.env.SMPT_PASSWORD,
+    //     },
+    // });
 
-    await transporter.sendMail(mailOptions);
+    // const mailOptions = {
+    //     from: process.env.SMPT_MAIL,
+    //     to: options.email,
+    //     subject: options.subject,
+    //     html: options.message,
+    //};
+
+    //await transporter.sendMail(mailOptions);
 
     const mssg = {
         to: options.email,
