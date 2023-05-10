ec443e8ba6aa0574b89c56fee88da43ab0db3ee8
Mohit Kumawat <115658001+mo-heatt@users.noreply.github.com>
2023-05-10 22:10:35 +0000

    paymentControllerUpdate

--- backend/controllers/paymentController.js
+++ backend/controllers/paymentController.js
@@ -26,4 +26,32 @@ const { stripVTControlCharacters } = require('util');
 //     });
 //  })
 
+exports.processPayment = asyncErrorHandler(async (req,res,next)=>{
+    const {amount, email, phoneNo} = req.body;
+    var params = {};
 
+params["MID"] = process.env.PAYTM_MID;
+params["WEBSITE"] = process.env.PAYTM_WEBSITE;
+params["CHANNEL_ID"] = process.env.PAYTM_CHANNEL_ID;
+params["INDUSTRY_TYPE_ID"] = process.env.PAYTM_INDUSTRY_TYPE;
+params["ORDER_ID"] = "oid" + uuidv4();
+params["CUST_ID"] = process.env.PAYTM_CUST_ID ;
+params["TXN_AMOUT"] = JSON.stringify(amount);
+//params["CALLBACK_URL"] = `${req.protoco}://${req.get("host")}/api/v1/callback`;
+params["CALLBACK_URL"] = `https://${req.get("host")}/api/v1/callback`;
+params["EMAIL"] = email;
+params["MOBILE_NO"] = phoneNo;
+
+let paytmCheckSum = paytm.generateSignature(params,process.env.PAYTM_MERCHANT_KEY);
+paytmCheckSum.then(function (checksum){
+    let paytmParams = {
+        ...params,
+        "CHECKSUMHASH": checksum,
+    };
+    res.status(200).json({
+        paytmParams,
+    });
+}).catch(function (error){
+    console.log(error);
+});
+});
